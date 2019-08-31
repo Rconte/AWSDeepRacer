9434a778f8966839d28eb887e34c0970b25bb3f5
Rconte <46374468+Rconte@users.noreply.github.com>
2019-08-31 18:26:05 +0200

    Create reward_3.py

---
+++ reward_3.py
@@ -0,0 +1,23 @@
+def reward_function(params):
+
+    all_wheels_on_track = params['all_wheels_on_track']
+    distance_from_center = params['distance_from_center']
+    speed = params['speed']
+    track_width = params['track_width']
+    steering = abs(params['steering_angle'])
+    progress = params['progress']
+    reward = 1e-3
+    speed_threshold = 1.5
+    
+    if all_wheels_on_track and (0.5*track_width - distance_from_center) >= 0.05:
+        reward = 1.0
+        if speed > speed_threshold:
+            reward += progress
+    
+    ABS_STEERING_THRESHOLD = 15
+    if steering > ABS_STEERING_THRESHOLD:
+        reward *= 0.8
+    
+    
+
+    return float(reward)
